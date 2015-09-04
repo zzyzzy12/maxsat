a6928f10614a7150c56c57b25a4d987edababf6d
zzy <zzyzzy12@126.com>
2015-09-04 12:00:25 +0800

    correct initial.h

--- initial.h
+++ initial.h
@@ -7,27 +7,27 @@
 #include<set>
 #include<queue>
 #include<stack>
-using namespace std;
+using namespace std; 
 bool isNum(char c){
-	if (c>='0' && c<='9') return true;
-	if (c=='-') return true;
-	return false;
+  if (c>='0' && c<='9') return true;
+  if (c=='-') return true;
+  return false;
 } 
 void initial(int &n,int &m,int *ans,set<int> *C){ //读取数据
-	char s[1005];
-	scanf("%d%d",&n,&m);
+  char s[1005];
+  scanf("%d%d\n",&n,&m);
     for (int t=1;t<=m;t++){
-    	gets(s);
+      gets(s);
         int len=strlen(s);
         C[t].clear();
         for (int i=0;i<len;i++){
-          	if (!isNum(s[i])) continue;
-          	int k=1;
-          	if (s[i++]=='-') k=-1;
-          	int x=0;
-          	while (isNum(s[i])) x=x*10+s[i++]-'0';
-          	x*=k;
-          	C[t].insert(x);
+            if (!isNum(s[i])) continue;
+            int k=1;
+            if (s[i]=='-') k=-1,i++;
+            int x=0;
+            while (isNum(s[i])) x=x*10+s[i++]-'0';
+            x*=k;
+            C[t].insert(x);
         }
     }
 }
